76c2bc15f48cf613a89d3b985e344657f36c1ea8
stepit <stefanofrancesco.pitton@gmail.com>
2024-02-02 01:44:56 +0100

    add tests for error cases in create deal

--- contracts/market/src/tests/create_deal.rs
+++ contracts/market/src/tests/create_deal.rs
@@ -1,7 +1,7 @@
 use cosmwasm_std::{Addr, Coin, Decimal, Empty, Uint128};
 use cw_multi_test::{App, BankSudo, Contract, ContractWrapper, Executor, SudoMsg};
 
-use crate::msg::{AllDealsResponse, DealsByCreatorResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::{error::ContractError, msg::{AllDealsResponse, DealsByCreatorResponse, ExecuteMsg, InstantiateMsg, QueryMsg}};
 
 const OWNER: &str = "0xstepit000";
 
@@ -40,7 +40,7 @@ fn create_deal_works() {
             None
     ).unwrap();
 
-    // Mint tokens to deals creators.
+    // Mint tokens to two accounts.
     let coin = Coin {
         denom: "astro".to_string(),
         amount: Uint128::new(1_000),
@@ -112,4 +112,101 @@ fn create_deal_works() {
 
     assert_eq!(resp.deals.len(), 0, "expected zero deal from first creator because expired");
     assert_eq!(resp_all.deals.len(), 1, "expected one deal still active");
+}
+
+#[test]
+fn create_deal_handle_errors() {
+    let mut app: App = App::default();
+
+    let owner = Addr::unchecked(OWNER);
+    let not_a_scammer = Addr::unchecked("0xtrustme".to_string());
+
+    // Store and instantiate the market contract.
+    let market_id = app.store_code(market_contract());
+    let init_market_msg = InstantiateMsg {
+        first_coin: "astro".to_string(),
+        second_coin: "usdc".to_string(),
+        fee: Decimal::percent(2),
+    };
+    let market_addr = app
+        .instantiate_contract(
+            market_id, 
+            owner.clone(), 
+            &init_market_msg,
+            &[], 
+            "otc-market", 
+            None
+    ).unwrap();
+
+    // Mint tokens to two accounts.
+    let mut coin = Coin {
+        denom: "astro".to_string(),
+        amount: Uint128::new(1_000),
+    };
+    app.sudo(
+        SudoMsg::Bank(BankSudo::Mint {
+            to_address: not_a_scammer.to_string(),
+            amount: vec![coin.clone()],
+    })).unwrap();
+    coin.denom = "osmo".to_string();
+    app.sudo(
+        SudoMsg::Bank(BankSudo::Mint {
+            to_address: not_a_scammer.to_string(),
+            amount: vec![coin],
+    })).unwrap();
+
+    // The deal is valid
+    let create_deal_msg = ExecuteMsg::CreateDeal { 
+        coin_out: Coin::new(1_000, "usdc"), 
+        counterparty: None, 
+        timeout: 10 
+    };
+    let err = app.execute_contract(
+        not_a_scammer.clone(), 
+        market_addr.clone(), 
+        &create_deal_msg, 
+        &[Coin::new(1_000, "astro"), Coin::new(1_000, "osmo")]
+    ).unwrap_err();
+
+    assert_eq!(
+        err.downcast_ref::<ContractError>().unwrap(), 
+        &ContractError::FundsError { }, 
+        "expected error because sent two coins"
+    );
+
+    let create_deal_msg = ExecuteMsg::CreateDeal { 
+        coin_out: Coin::new(1_000, "osmo"), 
+        counterparty: None, 
+        timeout: 100
+    };
+    let err = app.execute_contract(
+        not_a_scammer.clone(), 
+        market_addr.clone(), 
+        &create_deal_msg, 
+        &[Coin::new(1_000, "astro")]
+    ).unwrap_err();
+
+    assert_eq!(
+        err.downcast_ref::<ContractError>().unwrap(), 
+        &ContractError::CoinNotAllowed {  }, 
+        "expected error because output coin not allowed"
+    );
+
+    let create_deal_msg = ExecuteMsg::CreateDeal { 
+        coin_out: Coin::new(1_000, "astro"), 
+        counterparty: None, 
+        timeout: 100
+    };
+    let err = app.execute_contract(
+        not_a_scammer.clone(), 
+        market_addr.clone(), 
+        &create_deal_msg, 
+        &[Coin::new(1_000, "osmo")]
+    ).unwrap_err();
+
+    assert_eq!(
+        err.downcast_ref::<ContractError>().unwrap(), 
+        &ContractError::CoinNotAllowed {  }, 
+        "expected error because sent coin not allowed"
+    );
 }
